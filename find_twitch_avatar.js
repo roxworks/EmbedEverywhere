0e584c48f023b6cae2ac07c40a960cdb3440379e
Nogweii <me@nogweii.net>
2021-08-22 06:34:30 -0700

    remove image from json data

--- find_twitch_avatar.js
+++ find_twitch_avatar.js
@@ -1,14 +1,25 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const data = require('./data');
-
-console.log(`https://twitch.tv/${data.twitch_name}`);
-
-(async () => {
-  const response = await fetch(`https://twitch.tv/${data.twitch_name}`);
+async function twitch_ava(name) {
+  const response = await fetch(`https://twitch.tv/${name}`);
   const body = await response.text();
   const $ = cheerio.load(body);
 
-  console.log($('meta[property="og:image"]').attr("content"));
-})();
+  var twitch_profile_url = $('meta[property="og:image"]').attr("content");
+
+  return twitch_profile_url;
+}
+
+exports.twitch_ava = twitch_ava
+
+/*
+(async () => {
+  const data = require('./data');
+
+  console.log(`https://twitch.tv/${data.twitch_name}`);
+
+  url = twitch_ava(data.twitch_name)
+
+  console.log(url);
+})(); */
