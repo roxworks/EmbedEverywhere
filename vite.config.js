cbc725efc6ce1ed950e861b9cbbd2b013ffb7d73
Nogweii <me@nogweii.net>
2021-08-24 18:38:07 -0700

    initial vite configuration

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+const { resolve } = require('path');
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  root: resolve(__dirname, 'src/'),
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        form: resolve(__dirname, 'src/form.html'),
+      }
+    }
+  }
+})
+
